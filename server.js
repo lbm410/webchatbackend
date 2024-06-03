f63916f0468182724fcaf8334a0052a13cf33815
Lucas Barrientos <lbm410@inlumine.ual.es>
2024-06-03 13:19:00 +0200

    Mongoose actualizacion

--- server.js
+++ server.js
@@ -10,12 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
+
 
 // Importar rutas
 const userRoutes = require('./routes/userRoutes');
